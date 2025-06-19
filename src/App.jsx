efe095409fe884981675e7199c98f3f0672f4684
Stella-055 <stellaongayo243@gmail.com>
2025-06-19 15:13:20 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1>TODO APPLICATION</h1>
+     <div>
+     
+      <input type="text"  placeholder='task eg.Read'/>
+      <input type="text" placeholder='Description eg Read about trees' />
+    <button>ADD</button>
+     </div>
+
+    </>
+  )
+}
+
+export default App
