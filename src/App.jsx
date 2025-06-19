9e730a1ea342b8734205ae4b1a32147f9d804bb7
Stella-055 <stellaongayo243@gmail.com>
2025-06-19 16:05:54 +0300

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,66 @@
-import { useState } from 'react'
+import { use, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-  return (
-    <>
-     <h1>TODO APPLICATION</h1>
-     <div>
-     
-      <input type="text"  placeholder='task eg.Read'/>
-      <input type="text" placeholder='Description eg Read about trees' />
-    <button>ADD</button>
-     </div>
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+  const [alltasks, setAlltasks] = useState([
+    { task: "read", description: "Read about trees" },
+  ]);
 
-    </>
-  )
+  function Additems(e) {
+    e.preventDefault();
+    if (task === "" || description === "") {
+      alert("Please fill in both fields");
+      return;
+    }
+    const newtask = {
+      task: task,
+      description: description,
+    };
+    setAlltasks([...alltasks, newtask]);
+    setTask("");
+    setDescription("");
+  }
+  return (
+    <div className="App">
+      <h1>TODO APPLICATION</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="task eg.Read"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Description eg Read about trees"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={Additems}>ADD</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {alltasks.map((task, index) => {
+            return (
+              <tr key={index}>
+                <td>{task.task}</td>
+                <td>{task.description}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
